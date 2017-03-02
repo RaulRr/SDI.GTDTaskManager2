bb253362f3e9072c552b7d874019fec921fa5ca5
RaulRr <raulr439@hotmail.com>
2017-03-02 19:35:12 +0100

    Primer intento listadoUsuarios. Añadido properties para los idiomas

--- NotaneitorFinal/src/com/sdi/presentation/BeanUser.java
+++ NotaneitorFinal/src/com/sdi/presentation/BeanUser.java
@@ -1,4 +1,5 @@
 package com.sdi.presentation;
+
 import java.io.Serializable;
 
 import javax.faces.bean.*;
@@ -10,54 +11,76 @@ import com.sdi.business.exception.BusinessException;
 import com.sdi.dto.User;
 import com.sdi.dto.types.UserStatus;
 
-@ManagedBean(name="controller")
+@ManagedBean(name = "controller")
 @SessionScoped
-public class BeanUser implements Serializable{
-	
+public class BeanUser implements Serializable {
+
 	private static final long serialVersionUID = 55555L;
-	
-	//Se aÃ±ade este atributo de entidad para recibir el alumno concreto selecionado de
-	//la tabla o de un formulario
-	// Es necesario inicializarlo para que al entrar desde el formulario de AltaForm.xml
-	//se puedan
+
+	// Se aÃ±ade este atributo de entidad para recibir el alumno concreto
+	// selecionado de
+	// la tabla o de un formulario
+	// Es necesario inicializarlo para que al entrar desde el formulario de
+	// AltaForm.xml
+	// se puedan
 	// dejar los avalores en un objeto existente.
-	
+
 	private User user = new User();
 
-	public BeanUser()
-	{
+	private User[] users = null;
+
+	public BeanUser() {
 		iniciaUser(null);
 	}
-	
-	//Inicia el usuario pero aún no tiene valores
+
+	// Inicia el usuario pero aún no tiene valores
 	public void iniciaUser(ActionEvent event) {
 		user.setId(null);
 		user.setLogin("login");
 		user.setPassword("password");
 		user.setIsAdmin(false);
 		user.setEmail("email");
-		user.setStatus(UserStatus.DISABLED); 
+		user.setStatus(UserStatus.DISABLED);
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User alumno) {
 		this.user = alumno;
 	}
-	
-	public String validar(){
+
+	public User[] getUsers() {
+		return users;
+	}
+
+	public void setUsers(User[] users) {
+		this.users = users;
+	}
+
+	public String validar() {
 		UserService userService;
-		try{
+
+		try {
 			userService = Services.getUserService();
 			user = userService.findLoggableUser(user.getLogin(),
 					user.getPassword());
-		
-		}catch(BusinessException e){
-			return "error";
+			if (user.getIsAdmin()) { // Si es admin muestra la lista usuarios
+				listadoUsuarios();
+				return "admin";
+			}
+
+		} catch (BusinessException e) {
+			return "error"; //Se produjo algún error al validar
 		}
-		
-		return "exito";
+
+		return "user"; //Es un usario normal
 	}
-	
+
+	private void listadoUsuarios() throws BusinessException {
+		users = (User[]) Services.getAdminService().findAllUsers()
+				.toArray(new User[0]);
+	}
+
 }
