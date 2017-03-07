2c8ecb39f82f91882b9663e1efdee3ede6b93b5e
rotura <enolmbgi26@gmail.com>
2017-03-07 20:02:39 +0100

    Completado crear usuario, añadidos botones atrás

--- NotaneitorFinal/src/com/sdi/presentation/BeanUser.java
+++ NotaneitorFinal/src/com/sdi/presentation/BeanUser.java
@@ -2,8 +2,10 @@ package com.sdi.presentation;
 
 import java.io.Serializable;
 import java.util.List;
+
 import javax.faces.bean.*;
 import javax.faces.event.ActionEvent;
+
 import com.sdi.business.AdminService;
 import com.sdi.business.Services;
 import com.sdi.business.UserService;
@@ -29,8 +31,18 @@ public class BeanUser implements Serializable {
 	private User user = new User();
 
 	private List<User> users = null;
-	
+
 	private List<Task> tasks = null;
+
+	private String pass = "";
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 
 	public BeanUser() {
 		iniciaUser(null);
@@ -65,11 +77,11 @@ public class BeanUser implements Serializable {
 	public List<Task> getTasks() {
 		return tasks;
 	}
-	
+
 	public void setTasks(List<Task> tasks) {
 		this.tasks = tasks;
 	}
-	
+
 	public String validar() {
 		UserService userService;
 
@@ -81,74 +93,108 @@ public class BeanUser implements Serializable {
 				listadoUsuarios();
 				return "admin";
 			}
-			
-			listadoTareas();//Si usuario cargamos las tareas desde el inbox
-			
+
+			listadoTareas();// Si usuario cargamos las tareas desde el inbox
+
 		} catch (BusinessException b) {
-			return "error"; //Se produjo algún error al validar
+			return "error"; // Se produjo algún error al validar
 		}
-		
-		return "user"; //Es un usario normal
+
+		return "user"; // Es un usario normal
 	}
-	
-	public String cambiarEstado(User user){
+
+	public String cambiarEstado(User user) {
 		AdminService adminService;
-		if(user == null)
+		if (user == null)
 			return "error";
-		try{
-			adminService= Services.getAdminService();
-			if(user !=null && user.getStatus().equals(UserStatus.ENABLED)){
+		try {
+			adminService = Services.getAdminService();
+			if (user != null && user.getStatus().equals(UserStatus.ENABLED)) {
 				adminService.disableUser(user.getId());
-			}
-			else{
+			} else {
 				adminService.enableUser(user.getId());
 			}
-			listadoUsuarios(); //Actualizamos la lista de usuarios
-			return "exito"; //Nos volvemos al listado
-		}catch(BusinessException b){
+			listadoUsuarios(); // Actualizamos la lista de usuarios
+			return "exito"; // Nos volvemos al listado
+		} catch (BusinessException b) {
 			return "error";
 		}
 	}
-	
-	public String eliminar(User user){
+
+	public String eliminar(User user) {
 		AdminService adminService;
-		if(user == null)
+		if (user == null)
 			return "error";
-		try{
+		try {
 			adminService = Services.getAdminService();
 			adminService.deepDeleteUser(user.getId());
 			listadoUsuarios();
-			return "exito"; //Nos volvemos al listado
-		} catch (BusinessException b){
+			return "exito"; // Nos volvemos al listado
+		} catch (BusinessException b) {
 			return "error";
 		}
 	}
 
 	private void listadoUsuarios() throws BusinessException {
-		users =  Services.getAdminService().findAllUsers();
+		users = Services.getAdminService().findAllUsers();
 	}
-	
-	private void listadoTareas() throws BusinessException{
+
+	private void listadoTareas() throws BusinessException {
 		tasks = Services.getTaskService().findInboxTasksByUserId(user.getId());
 	}
-	
-	public String registro(){
+
+	public String registro() {
 		return "true";
 	}
-	
+
 	public String registrar() {
 		UserService userService;
+
+		if (!user.getEmail().matches("[-\\w\\.]+@\\w+\\.\\w+")) {
+			System.out.println("Email invalido: " + user.getEmail());
+			return "false";
+		}
+
+		if (!pass.equals(user.getPassword())) {
+			System.out.println("Las contraseñas no coinciden: " + pass + " - "
+					+ user.getPassword());
+			return "false";
+		}
+
+		else {
+			if (pass.length()<8) {
+				System.out
+						.println("Las contraseñas deben medir al menos 8 caracteres "
+								+ pass);
+				return "false";
+			}
+			
+			if (!pass.matches(".*[a-zA-Z].*") || !pass.matches(".*[0-9].*")) {
+				System.out
+						.println("Las contraseñas no contiene letras y números "
+								+ pass);
+				return "false";
+			}
+		}
 
 		try {
 			userService = Services.getUserService();
 			user.setIsAdmin(false);
 			user.setStatus(UserStatus.ENABLED);
 			userService.registerUser(user);
-			
+			user.setEmail(null);
+			user.setPassword(null);
+			user.setIsAdmin(false);
+			user.setId(null);
 		} catch (BusinessException b) {
-			return "error"; //Se produjo algún error al validar
+			return "false"; // Se produjo algún error al validar
 		}
-		
-		return "true"; //Es un usario normal
+
+		return "true"; // Es un usario normal
+	}
+
+	public String atras() {
+		System.out.println("Pulsado atras");
+		return "true";
 	}
 }
